87cea199649ed405d554a2e833e38b4b6f8cbc91
jefersonmarin <jefersong.marin2002@gmail.com>
2025-02-10 10:08:46 -0300

    Fix baseURL route

--- mobile/src/services/api.ts
+++ mobile/src/services/api.ts
@@ -15,7 +15,7 @@ type APIInstanceProps = AxiosInstance & {
 }
 
 const api = axios.create({
-  baseURL: 'http://192.168.100.30:3333',
+  baseURL: 'http://172.17.172.140:3333',
 }) as APIInstanceProps;
 
 let failedQueued: Array<PromiseType> = [];
